2d9439edd5677b6cd6ce0a10e875fb01aab80205
Ikhwan A. Latif <ikhwanal235@gmail.com>
2026-03-05 10:19:48 +0700

    refactor: remove magic number for range value in the main function

--- program/main.js
+++ program/main.js
@@ -1,15 +1,17 @@
 function main() {
+  const range = 5000
+
   let generator = new Map()
 
   let selfNumber = []
 
-  for (let i = 0; i < 5000; i++) {
+  for (let i = 0; i < range; i++) {
     const total = d(i)
 
     generator.set(total, generator.get(total) || 0 + 1)
   }
 
-  for (let i = 0; i < 5000; i++) {
+  for (let i = 0; i < range; i++) {
     if (!generator.has(i)) {
       selfNumber.push(i)
     }
